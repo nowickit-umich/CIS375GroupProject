5a725b74c06bd9631c8f377675b871cede4d36b7
nowickit-umich <nowickit@umich.edu>
2024-10-01 02:51:20 -0400

    Updated server scripts

--- server/setup.sh
+++ server/setup.sh
@@ -37,10 +37,26 @@ nft add rule ip filter input udp dport 500 accept
 nft add rule ip filter input udp dport 4500 accept
 
 #Save Firewall Config
-#nft list ruleset > /etc/nftables.conf
+nft list ruleset > /etc/nftables.conf
 
 #Configure VPN
-#curl -o /etc/ipsec.conf https://github.com/nowickit-umich/CIS375GroupProject/blob/main/server/config/ipsec.conf
+curl -o /etc/swanctl/swanctl.conf https://raw.githubusercontent.com/nowickit-umich/CIS375GroupProject/refs/heads/main/server/config/swanctl.conf
+
+#VPN Certificate config
+#TODO
+curl -o /root/ca.conf https://raw.githubusercontent.com/nowickit-umich/CIS375GroupProject/refs/heads/main/server/config/cert/ca.conf
+curl -o /root/server.conf https://raw.githubusercontent.com/nowickit-umich/CIS375GroupProject/refs/heads/main/server/config/cert/server.conf
+
+openssl rand 19 > /root/serial
+openssl genpkey -algorithm RSA -out /root/ca-key.pem -pkeyopt rsa_keygen_bits:4096
+openssl req -x509 -new -noenc -key ca-key.pem -sha256 -days 3650 -out /etc/swanctl/x509ca/ca-cert.pem -config /root/ca.conf
+openssl genpkey -algorithm RSA -out /etc/swanctl/private/server-key.pem -pkeyopt rsa_keygen_bits:4096
+openssl req -new -key /etc/swanctl/private/server-key.pem -outform pem -out server-req.pem
+openssl ca -in server-req.pem -inform pem -config server.conf
 
 systemctl restart strongswan
 systemctl enable strongswan
+
+
+#Web server setup
+
