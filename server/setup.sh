f95f76aefadb0dbad3ba079c30f9e541968641d1
nowickit-umich <nowickit@umich.edu>
2024-11-06 20:27:53 -0500

    setup bug fix

--- server/setup.sh
+++ server/setup.sh
@@ -3,7 +3,7 @@
 #VPN server setup
 #Run as root
 
-CLIENT_NET="10.99.99.0/24"
+CLIENT_NET="10.99.99.0\/24"
 SERVER_INTERFACE="enX0"
 LOCAL_ADDR=$(hostname -I)
 SPATH=$(dirname $(realpath "$0"))
@@ -20,7 +20,7 @@ cp $SPATH/templates/dnsmasq.template $SPATH/config/dnsmasq.conf
 sed -i -e "s/%SERVER_INTERFACE%/$SERVER_INTERFACE/g" $SPATH/config/dnsmasq.conf
 cp $SPATH/config/dnsmasq.conf /etc/dnsmasq.d/dnsmasq.conf
 systemctl enable dnsmasq
-systemctl start dnsmasq
+systemctl restart dnsmasq
 
 #Enable forwarding
 if [ ! -f /etc/sysctl.d/forward.conf ]; then
@@ -35,7 +35,7 @@ cp $SPATH/templates/nftables.template $SPATH/config/nftables.conf
 sed -i -e "s/%CLIENT_NET%/$CLIENT_NET/g" $SPATH/config/nftables.conf
 sed -i -e "s/%SERVER_INTERFACE%/$SERVER_INTERFACE/g" $SPATH/config/nftables.conf
 cp $SPATH/config/nftables.conf /etc/nftables.conf
-/usr/sbin/nft -f nftables.conf
+/usr/sbin/nft -f /etc/nftables.conf
 
 #Get server Public IP
 IP="$(curl -s ifconfig.me)"
@@ -67,15 +67,14 @@ sed -i -e "s/%LOCAL_ADDR%/$LOCAL_ADDR/g" $SPATH/config/swanctl.conf
 cp $SPATH/config/swanctl.conf /etc/swanctl/swanctl.conf
 cp $SPATH/config/charon-systemd.conf /etc/strongswan.d/charon-systemd.conf
 
+#Certificate Config
 cp $SPATH/templates/cert.template $SPATH/config/cert.conf
 sed -i -e "s/%IP%/$IP/g" $SPATH/config/cert.conf
-
-#Certificate Config
 openssl req -x509 -newkey rsa:4096 -keyout $SPATH/key.pem -out $SPATH/cert.pem -sha256 -days 3650 -nodes -config $SPATH/config/cert.conf
-
 mv $SPATH/key.pem /etc/swanctl/private/key.pem
 mv $SPATH/cert.pem /etc/swanctl/x509/cert.pem
 
+#Start VPN
 systemctl restart strongswan
 systemctl enable strongswan
 
