b99d074245255864ae287fa11909587b8456ef43
nowickit-umich <159186542+nowickit-umich@users.noreply.github.com>
2024-11-14 17:09:58 -0500

    Update aws_interface.py

--- client/cloud/aws_interface.py
+++ client/cloud/aws_interface.py
@@ -18,66 +18,155 @@ class AwsInterface(CloudInterface):
         resource = self.session.resource('ec2', region_name=region)
         client = self.session.client('ec2', region_name=region)
 
+
+        #response = client.describe_security_groups(GroupNames=["ALLOW_VPN"])
+        
+
         #Configure security group - input firewall
         response = client.create_security_group(
-                Description='vpn security group',
-                GroupName='TESTALLOWVPN',
-                TagSpecifications=[{'ResourceType':'security-group','Tags':[{'Key':'cisvpn','Value':'delete'}]}],
-                DryRun=True
-            )
+            Description='vpn security group',
+            GroupName='TESTALLOWVPN',
+            TagSpecifications=[{'ResourceType':'security-group','Tags':[{'Key':'cisvpn','Value':'delete'}]}],
+            DryRun=False
+        )
         if response == 0:
             #TODO Error handling
             pass
         sec_group_id = response["GroupId"]
 
-        #TODO - Not done - specify all rules in a single API call
+        # Allow VPN and SSH traffic inbound
         response = client.authorize_security_group_ingress(
-                GroupId=sec_group_id,
-                IpPermissions=[
-                    {
-                        'IpProtocol': 'udp',
-                        'FromPort': -1,
-                        'ToPort': 500,
-                        'IpRanges': [
-                            {
-                                'Description': 'VPN500',
-                                'CidrIp': 'string'
-                            },
-                        ],
-                        'Ipv6Ranges': [
-                            {
-                                'Description': 'string',
-                                'CidrIpv6': 'string'
-                            },
-                        ],
-                        'PrefixListIds': [
-                            {
-                                'Description': 'string',
-                                'PrefixListId': 'string'
-                            },
-                        ]
-                    },
-                ],
-                IpProtocol='string',
-                SourceSecurityGroupName='string',
-                SourceSecurityGroupOwnerId='string',
-                ToPort=123,
+            GroupId=sec_group_id,
+            IpPermissions=[
+                {
+                    'IpProtocol': 'tcp',
+                    'FromPort': 22,
+                    'ToPort': 22,
+                    'IpRanges': [
+                        {
+                            'CidrIp': '0.0.0.0/0'
+                        },
+                    ],
+                    'Ipv6Ranges': [],
+                    'PrefixListIds': []
+                },
+                {
+                    'IpProtocol': 'udp',
+                    'FromPort': 500,
+                    'ToPort': 500,
+                    'IpRanges': [
+                        {
+                            'CidrIp': '0.0.0.0/0'
+                        },
+                    ],
+                    'Ipv6Ranges': [],
+                    'PrefixListIds': []
+                },
+                {
+                    'IpProtocol': 'udp',
+                    'FromPort': 4500,
+                    'ToPort': 4500,
+                    'IpRanges': [
+                        {
+                            'CidrIp': '0.0.0.0/0'
+                        },
+                    ],
+                    'Ipv6Ranges': [],
+                    'PrefixListIds': []
+                },
+            ],
+            DryRun=False
+        )
+        
+        # Create SSH Key
+        ssh_key = client.create_key_pair(
+                KeyName='cisvpn',
+                KeyType='ed25519',
                 TagSpecifications=[
                     {
-                        'ResourceType': 'capacity-reservation'|'client-vpn-endpoint'|'customer-gateway'|'carrier-gateway'|'coip-pool'|'dedicated-host'|'dhcp-options'|'egress-only-internet-gateway'|'elastic-ip'|'elastic-gpu'|'export-image-task'|'export-instance-task'|'fleet'|'fpga-image'|'host-reservation'|'image'|'import-image-task'|'import-snapshot-task'|'instance'|'instance-event-window'|'internet-gateway'|'ipam'|'ipam-pool'|'ipam-scope'|'ipv4pool-ec2'|'ipv6pool-ec2'|'key-pair'|'launch-template'|'local-gateway'|'local-gateway-route-table'|'local-gateway-virtual-interface'|'local-gateway-virtual-interface-group'|'local-gateway-route-table-vpc-association'|'local-gateway-route-table-virtual-interface-group-association'|'natgateway'|'network-acl'|'network-interface'|'network-insights-analysis'|'network-insights-path'|'network-insights-access-scope'|'network-insights-access-scope-analysis'|'placement-group'|'prefix-list'|'replace-root-volume-task'|'reserved-instances'|'route-table'|'security-group'|'security-group-rule'|'snapshot'|'spot-fleet-request'|'spot-instances-request'|'subnet'|'subnet-cidr-reservation'|'traffic-mirror-filter'|'traffic-mirror-session'|'traffic-mirror-target'|'transit-gateway'|'transit-gateway-attachment'|'transit-gateway-connect-peer'|'transit-gateway-multicast-domain'|'transit-gateway-policy-table'|'transit-gateway-route-table'|'transit-gateway-route-table-announcement'|'volume'|'vpc'|'vpc-endpoint'|'vpc-endpoint-connection'|'vpc-endpoint-service'|'vpc-endpoint-service-permission'|'vpc-peering-connection'|'vpn-connection'|'vpn-gateway'|'vpc-flow-log'|'capacity-reservation-fleet'|'traffic-mirror-filter-rule'|'vpc-endpoint-connection-device-type'|'verified-access-instance'|'verified-access-group'|'verified-access-endpoint'|'verified-access-policy'|'verified-access-trust-provider'|'vpn-connection-device-type'|'vpc-block-public-access-exclusion'|'ipam-resource-discovery'|'ipam-resource-discovery-association'|'instance-connect-endpoint'|'ipam-external-resource-verification-token',
+                        'ResourceType': 'key-pair',
                         'Tags': [
                             {
-                                'Key': 'string',
-                                'Value': 'string'
-                            },
+                                'Key': 'cisvpn',
+                                'Value': 'delete'
+                            }
                         ]
-                    },
+                    }
                 ],
-                DryRun=True
+                KeyFormat='pem',
+                DryRun=False
+            )
+
+        # Get image (AMI) id
+        
+
+        resource.create_instances(
+                BlockDeviceMappings=[{
+                    'Ebs': {
+                        'DeleteOnTermination': True,
+                        'Iops': 3000,
+                        'VolumeSize': 5,
+                        'VolumeType': 'gp3',
+                        'Throughput': 125,
+                        'Encrypted': False
+                    },
+                    'DeviceName': 'xvda',
+                }],
+                ImageId=IMAGEID,
+                InstanceType='t2.micro',
+                KeyName=SSHKEY,
+                MaxCount=1,
+                MinCount=1,
+                Monitoring={'Enabled':False},
+                #Placement={},
+                #RamdiskId='',
+                #SecurityGroupIds=[SECGROUPID],
+                #SecurityGroups=[],
+                #SubnetId='',
+                #UserData='',
+                #ElasticGpuSpecification=[],
+                #ElasticInferenceAccelerators=[],
+                TagSpecifications=['ResourceType':'instance', 'Tags':[{'Key':'cisvpn','Value':'delete'}]],
+                #LaunchTemplate={},
+                #InstanceMarketOptions={},
+                CreditSpecification={'CpuCredits':'standard'},
+                #CpuOptions={},
+                CapacityReservationSpecification={'CapacityReservationPreference':'none'},
+                #HibernationOptions={},
+                #LicenseSpecifications={},
+                #MetadataOptions={},
+                #EnclaveOptions={},
+                #PrivateDnsNameOptions={},
+                #MaintenanceOptions={},
+                DisableApiStop=False,
+                EnablePrimaryIpv6=False,
+                DryRun=False,
+                DisableApiTermination=False,
+                InstanceInitiatedShutdownBehavior='terminate',
+                #PrivateIpAddress=,
+                #ClientToken=,
+                #AdditionalInfo=,
+                NetworkInterfaces=[{
+                    'AssociatePublicIpAddress': True,
+                    'DeleteOnTermination':True,
+                    'Description':'InterfaceDescription',
+                    'DeviceIndex':0,
+                    'Groups':[sec_group_id],
+                    'Ipv6AddressCount':0,
+                    #'NetworkInterfaceId':'',
+                    'PrivateIpAddress':'10.88.88.1/24',
+                    'SecondaryPrivateIpAddressCount':0,
+                    #'SubnetId':SUBNETID,
+                    'AssociateCarrierIpAddress':False,
+                    #'InterfaceType':?,
+                    'NetworkCardIndex':0,
+                    }],
+                #IamInstanceProfile={},
+                EbsOptimized=False
             )
         
 
-
+        quit()
         pass
     
     def delete_server(self):
