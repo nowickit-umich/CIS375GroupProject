404a8d0741c5a920d2f45d811d2eff1352afd283
unknown <nowickit@umich.edu>
2024-11-17 04:22:47 -0500

    server config

---
+++ server/server_cmd.py
@@ -0,0 +1,11 @@
+# listen on private ip for requests from client
+
+# enable/disable block list
+# add/remove blocklist
+# list block lists
+
+# return stats db
+
+# write dns logs into stats db - continuous
+
+# 
