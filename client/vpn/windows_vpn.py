613c7929880f8a8b0658eb41c00cef587d3cb1d1
zsbeih <156637869+zsbeih@users.noreply.github.com>
2024-11-25 15:26:13 -0500

    -1 fail, 0 success for debug calls

--- client/vpn/windows_vpn.py
+++ client/vpn/windows_vpn.py
@@ -27,17 +27,17 @@ lib.debug.restype = ctypes.c_int
 # Windows VPN Implementation
 class Windows_VPN(VPN_Interface):
     def init(self):
-        self.debug(1, "VPN Interface Initialized") # Successful Initalization(1 = success, 0 = fail)
+        self.debug(0, "VPN Interface Initialized") # Successful Initalization(0 = success, -1 = fail)
 
     def install_cert(self, path): # Install certificate given path
         try:
             cmd = f'-Command Import-Certificate -FilePath "{path}" -CertStoreLocation Cert:\LocalMachine\Root'
             admin_cmd = f'Powershell -Command Start-Process -Verb RunAs -WindowStyle Hidden -FilePath "PowerShell.exe" -ArgumentList "{cmd}"'
             subprocess.run(admin_cmd, creationflags=subprocess.CREATE_NO_WINDOW, shell=True, check=True)
-            self.debug(1, "Certificate Installed Successfully")
+            self.debug(0, "Certificate Installed Successfully")
 
         except Exception as e:
-            self.debug(0, f"Error: {e}")
+            self.debug(-1, f"Error: {e}")
             raise
 
     def create_profile(self, profile_name, server_address, pbk_path): # Create profile given name, server address, and pbk path
@@ -49,13 +49,13 @@ class Windows_VPN(VPN_Interface):
                 )
 
                 if ret != 0:
-                    self.debug(0, "Profile Creation Failed")
+                    self.debug(-1, "Profile Creation Failed")
                     return -1
-                self.debug(1, "Profile created successfully")
+                self.debug(0, "Profile created successfully")
                 return ret
             
             except Exception as e:
-                self.debug(0, f"Error: {e}")
+                self.debug(-1, f"Error: {e}")
                 return -1
             
     def debug(self, x, s): # Debug function printing out the string and error code
@@ -71,13 +71,13 @@ class Windows_VPN(VPN_Interface):
             )
 
             if ret != 0:
-                self.debug(0, "VPN Connection Failed")
+                self.debug(-1, "VPN Connection Failed")
                 return -1
-            self.debug(1, "VPN Profile Connected")
+            self.debug(0, "VPN Profile Connected")
             return ret
         
         except Exception as e:
-            self.debug(0, f"Error: {e}")
+            self.debug(-1, f"Error: {e}")
             return -1
 
     def disconnect(self, profile_name): # Disconnect VPN profile using name of profile
@@ -85,14 +85,14 @@ class Windows_VPN(VPN_Interface):
             ret = lib.disconnect_vpn(profile_name.encode('utf-8'))
 
             if ret != 0:
-                self.debug(0, "VPN Disconnection Failed")
+                self.debug(-1, "VPN Disconnection Failed")
                 return -1
             
-            self.debug(1, "VPN Profile Disconnected")
+            self.debug(0, "VPN Profile Disconnected")
             return ret
         
         except Exception as e:
-            self.debug(0, f"Error: {e}")
+            self.debug(-1, f"Error: {e}")
             return -1
         
     def status(self, profile_name): # Get status of profile using profile name
@@ -100,13 +100,13 @@ class Windows_VPN(VPN_Interface):
             ret = lib.status(profile_name.encode('utf-8'))
 
             if ret < 0:
-                self.debug(0, "Failed to Retrieve Status")
+                self.debug(-1, "Failed to Retrieve Status")
                 return -1
             
-            self.debug(1, "Status Retrieved Successfully")
+            self.debug(0, "Status Retrieved Successfully")
             return ret
         
         except Exception as e:
-            self.debug(0, f"Error: {e}")
+            self.debug(-1, f"Error: {e}")
             return -1
         
