5fd97041cbc6a8b20e7608a6e3d29bbacd2a2dd4
Zain Sbeih <zdogss488@gmail.com>
2024-11-26 19:37:53 -0500

    changed to txt file, updated block list format

--- client/filter_manager.py
+++ client/filter_manager.py
@@ -1,9 +1,17 @@
-import pickle 
 import paramiko
+
+# Filter_Manager class intended to add, delete, enable, disable, and send block lists to a server, the block lists are saved in this format:
+# sample_list = {
+#    'enabled': True,
+#    'domain_names': [
+#        {'name': 'www.google.com', 'block_mode': 'single_domain'},
+#        {'name': 'www.youtube.com', 'block_mode': 'all_subdomains'}
+#    ]
+#}
 
 class Filter_Manager:
     def __init__(self):
-        self.block_list = [] # List of dictionaries containing individual block lists
+        self.block_list = [] # List of dictionaries containing individual block lists with an 'enabled' key and 'domain_names' key 
         self.server_host = 'localhost'  
         self.server_port = 44444
 
@@ -25,9 +33,19 @@ class Filter_Manager:
         for list in self.block_list:
             if(list['enabled'] == True):
                 enabled_lists.append(list)
+        
+        with open('enabled_lists.txt', 'w') as f: # Loop over every enabled list and add an entry in the text file for each domain found in the list based on the specified block mode
+            for list in enabled_lists:
+                for domain in list['domain_names']:
+                    domain_name = domain['name']
+                    block_mode = domain['block_mode']
 
-        with open('enabled_lists.pkl', "wb") as f:
-            pickle.dump(enabled_lists, f)
+                    if block_mode == 'single_domain':
+                        blocked_domain = f"address={domain_name}/"
+                    else:  
+                        blocked_domain = f"address=/{domain_name}/"
+
+                    f.write(blocked_domain + '\n')
 
         try:
             
@@ -38,8 +56,8 @@ class Filter_Manager:
             ssh.connect(hostname=self.server_host, username="ubuntu", pkey=key)
             sftp = ssh.open_sftp()
             
-            sftp_file_path = "/etc/dnsmasq.d/enabled_lists.pkl"
-            sftp.put('enabled_lists.pkl', sftp_file_path)
+            sftp_file_path = "/etc/dnsmasq.d/enabled_lists.txt"
+            sftp.put('enabled_lists.txt', sftp_file_path)
             
             sftp.close()
             ssh.close()
