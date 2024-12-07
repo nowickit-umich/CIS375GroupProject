dada23833b2c1e3917f6abc9a4a97ef5c2a99351
unknown <nowickit@umich.edu>
2024-12-07 00:37:29 -0500

    dns bug fix

--- server/server_dns.py
+++ server/server_dns.py
@@ -4,11 +4,11 @@ import os
 # update dns filters
 def main():
     while(True):
-        if not os.path.isfile("/home/ubuntu/dnsmasq/flag"):
-            time.sleep(1)
-        os.remove("/home/ubuntu/dnsmasq/flag")
-        os.system("cp /home/ubuntu/dnsmasq/* /etc/dnsmasq.d/")
-        os.system("systemctl reload dnsmasq")  
+        if not os.path.exists("/home/ubuntu/dnsmasq/flag"):   
+            os.remove("/home/ubuntu/dnsmasq/flag")
+            os.system("cp /home/ubuntu/dnsmasq/* /etc/dnsmasq.d/")
+            os.system("systemctl reload dnsmasq")  
+        time.sleep(1)
 
 if __name__ == '__main__':
     main()
