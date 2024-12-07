ae01e053db27aedfd950022045dc7d65a51eaca4
unknown <nowickit@umich.edu>
2024-12-07 01:14:59 -0500

    update dns config

--- server/server_dns.py
+++ server/server_dns.py
@@ -7,7 +7,7 @@ def main():
         if os.path.exists("/home/ubuntu/dnsmasq/flag"):   
             os.remove("/home/ubuntu/dnsmasq/flag")
             os.system("cp /home/ubuntu/dnsmasq/* /etc/dnsmasq.d/")
-            os.system("systemctl reload dnsmasq")  
+            os.system("systemctl restart dnsmasq")  
         time.sleep(1)
 
 if __name__ == '__main__':
